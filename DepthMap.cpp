39162f2c8456473a985b6d2e3c6b0397db972d74
杨济宁 <695170418@qq.com>
2021-06-30 18:38:30 +0800

    支持了彩色点云

--- DepthMap.cpp
+++ DepthMap.cpp
@@ -206,44 +206,5 @@ void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>>&p
     return;
 }
 
-/*
-void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud) {
 
-    if (pointcloud.empty()) {
-        cerr << "Point cloud is empty!" << endl;
-        return;
-    }
-
-    pangolin::CreateWindowAndBind("Point Cloud", 1024, 768);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    pangolin::OpenGlRenderState s_cam(
-        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
-        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
-    );
-
-    pangolin::View &d_cam = pangolin::CreateDisplay()
-        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
-        .SetHandler(new pangolin::Handler3D(s_cam));
-
-    while (pangolin::ShouldQuit() == false) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        d_cam.Activate(s_cam);
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-        glPointSize(2);
-        glBegin(GL_POINTS);
-        for (auto &p: pointcloud) {
-            glColor3d(p[3] / 255.0, p[4] / 255.0, p[5] / 255.0);
-            glVertex3d(p[0], p[1], p[2]);
-        }
-        glEnd();
-        pangolin::FinishFrame();
-        usleep(5000);   // sleep 5 ms
-    }
-    return;
-}*/
 
